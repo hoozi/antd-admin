b343e125fb51dcb2f62d3e1a5265544765937dd8
AngryPanda <287036406@qq.com>
2018-10-12 17:08:14 +0800

    init

---
+++ config-overrides.js
@@ -0,0 +1,16 @@
+const { injectBabelPlugin } = require('react-app-rewired');
+const { resolve } = require('path');
+
+module.exports = function override(config, env) {
+  config = injectBabelPlugin(
+    ['import', { libraryName: 'antd', libraryDirectory: 'es', style: 'css' }], // change importing css to less
+    config,
+  );
+  let alias = config.resolve.alias;
+  alias['@'] = resolve(__dirname, './src');
+  alias['@c'] = resolve(__dirname, './src/components');
+  alias['@u'] = resolve(__dirname, './src/utils');
+  alias['@a'] = resolve(__dirname, './src/api');
+  alias['@p'] = resolve(__dirname, './src/pages');
+  return config;
+};
